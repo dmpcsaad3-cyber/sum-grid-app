a82240e02fad414ae8295706fd5108c6c7b51aa8
dmpcsaad3-cyber <dmpcsaad3@gmail.com>
2026-08-31 14:44:24 +0500

    Update Main activity.kt

--- android/app/src/main/kotlin/com/example/sum_grid_app/Main activity.kt
+++ android/app/src/main/kotlin/com/example/sum_grid_app/Main activity.kt
@@ -1,9 +1,5 @@
-<?xml version="1.0" encoding="utf-8"?>
-<resources>
-    <style name="LaunchTheme" parent="@android:style/Theme.Light.NoTitleBar">
-        <item name="android:windowBackground">@drawable/launch_background</item>
-    </style>
-    <style name="NormalTheme" parent="@android:style/Theme.Light.NoTitleBar">
-        <item name="android:windowBackground">?android:colorBackground</item>
-    </style>
-</resources>
+package com.example.sum_grid_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
