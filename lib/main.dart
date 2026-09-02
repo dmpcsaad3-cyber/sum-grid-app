e596e9614aaa10a1a94dc2e264c8e9ce012a34e5
dmpcsaad3-cyber <dmpcsaad3@gmail.com>
2026-09-02 09:59:53 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  Timer? _adTimer;
-
-  @override
-  void initState() {
-    super.initState();
-    // 15 sec baad pehla ad, phir HAR 1 MINUTE BAAD
-    Future.delayed(Duration(seconds: 15), () {
-      _openLink();
-      _adTimer = Timer.periodic(Duration(seconds: 60), (timer) { // YEH 1 MINUTE HAI
-        _openLink();
-      });
-    });
-  }
-
-  Future<void> _openLink() async {
-    final Uri link = Uri.parse('https://mahadplanner192.my.canva.site/');
-    if (await canLaunchUrl(link)) {
-      await launchUrl(link, mode: LaunchMode.externalApplication);
-    }
-  }
-
-  @override
-  void dispose() {
-    _adTimer?.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Sum Grid")),
-      body: Center(child: Text("Har 1 min baad auto site khulegi")),
-    );
-  }
+void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
+class Home extends StatefulWidget{@override State<Home> createState()=>_Home();}
+class _Home extends State<Home>{
+  Timer? t;
+  @override void initState(){super.initState();Future.delayed(Duration(seconds:15),(){_open();t=Timer.periodic(Duration(seconds:60),(_)=>_open());});}
+  _open() async{await launchUrl(Uri.parse('https://mahadplanner192.my.canva.site/'),mode:LaunchMode.externalApplication);}
+  @override void dispose(){t?.cancel();super.dispose();}
+  @override Widget build(BuildContext c){return Scaffold(appBar:AppBar(title:Text("Sum Grid")),body:Center(child:Text("Har 1 min baad link khulega")));}
 }
